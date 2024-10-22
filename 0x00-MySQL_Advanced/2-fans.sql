c76ce9368081db5286c7f453d246aa614185d40e
Nsikak-mbre <tm260715@gmail.com>
2024-10-23 00:11:16 +0100

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,19 @@
--- Import the table dump
-SOURCE /home/nk/alx-backend-storage/0x00-MySQL_Advanced/metal_bands.sql;
+DELIMITER //
 
--- Rank country origins of bands by the number of fans
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- Drop the procedure if it already exists
+DROP PROCEDURE IF EXISTS rank_country_origins;
+
+-- Create the procedure
+CREATE PROCEDURE rank_country_origins()
+BEGIN
+    -- Rank country origins of bands by the number of fans
+    SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
+END //
+
+DELIMITER ;
+
+-- Call the procedure
+CALL rank_country_origins();
