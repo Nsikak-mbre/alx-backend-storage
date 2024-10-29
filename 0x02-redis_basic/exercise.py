b492fd9c7f53aeb2e71357803411b5d3e0ca6df8
Nsikak-mbre <tm260715@gmail.com>
2024-10-29 06:43:47 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,6 +12,7 @@ class Cache:
     """
     Cache class to store data in Redis.
     """
+
     def __init__(self):
         """
         Initialize the Cache class, setting up a private Redis client instance
@@ -33,3 +34,46 @@ class Cache:
         rand_key = str(uuid.uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Optional[Callable]): A callable to convert the data to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data in the desired format or None if the key does not exist.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve a string value from Redis and decode it from bytes to UTF-8.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            str: The retrieved data as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve an integer value from Redis and convert it from bytes to an integer.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            int: The retrieved data as an integer.
+        """
+        return self.get(key, fn=int)
