644eb165419ff9a6962299611b029503b95730ff
Nsikak-mbre <tm260715@gmail.com>
2024-10-29 02:55:56 +0100

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Cache module for storing data in Redis.
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Cache class to store data in Redis.
+    """
+    def __init__(self):
+        """
+        Initialize the Cache class, setting up a private Redis client instance
+        and flushing the database.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a randomly generated key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store in Redis.
+
+        Returns:
+            str: The randomly generated key under which the data is stored.
+        """
+        rand_key = str(uuid.uuid4())
+        self._redis.set(rand_key, data)
+        return rand_key
